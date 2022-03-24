2350ae553e1362fd29202a6ed7a42ec155c9a558
himanshu0501 <himns0501@gmail.com>
2022-03-24 20:28:50 +0530

    basic app view is created ( drawer, app name )

---
+++ lib/homepage.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        // scaffold contains more functions than Material so better use Scaffold
+        appBar: AppBar(
+          // it gives the bar of App
+          title: Text("Space Curiosity"), // the name of App at the top
+        ),
+        body: Center(
+          // it is same as the HTML body
+          child: Container(
+            child: Text('welcome'),
+          ),
+        ),
+        drawer: Drawer(), // the side drawer is given here
+      ),
+    );
+  }
+}
